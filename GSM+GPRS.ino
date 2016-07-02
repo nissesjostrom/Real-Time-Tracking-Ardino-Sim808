a9ce0b7bba110bfae72905559ae8dc777095828b
Salman Faris <farissalmannbr@gmail.com>
2016-07-02 22:56:57 +0530

    Update GSM+GPRS.ino

--- GSM+GPRS.ino
+++ GSM+GPRS.ino
@@ -1,29 +1,4 @@
-
-/*  
- *  Where is my car? Realtime GPS+GPRS Tracking of Vehicles Using Arduino
- *  
- *  Copyright (C) Libelium Comunicaciones Distribuidas S.L. 
- *  http://www.libelium.com 
- *  
- *  This program is free software: you can redistribute it and/or modify 
- *  it under the terms of the GNU General Public License as published by 
- *  the Free Software Foundation, either version 3 of the License, or 
- *  (at your option) any later version. 
- *  a
- *  This program is distributed in the hope that it will be useful, 
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- *  GNU General Public License for more details.
- *  
- *  You should have received a copy of the GNU General Public License 
- *  along with this program.  If not, see http://www.gnu.org/licenses/. 
- *  
- *  Version:           1.0
- *  Design:            David Gascón 
- *  Implementation:    Marcos Martinez
- */
-
-int8_t answer;
+int 8_t answer;
 int onModulePin= 2;
 char aux_string[30];
 int flag = 0;
